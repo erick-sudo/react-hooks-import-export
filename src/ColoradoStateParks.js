79ddf118b71032c8bebe3e2a35449bb8b3fca9a0
Erick Ochieng <erickochieng766@gmail.com>
2023-01-10 12:59:24 +0300

    Connected

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde"
+import {trees, wildlife, elevation} from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(trees)
+  wildlife()
+  elevation()
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+      <MesaVerde />
+      <MesaVerde />
+    </div>
+  )
 }
+
+export default ColoradoStateParks
